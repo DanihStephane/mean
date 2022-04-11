6c82c4381bb3093ad59ad1d784010b86a1a3e19e
DanihStephane <92985744+DanihStephane@users.noreply.github.com>
2022-04-11 13:09:50 +0300

    change name database

--- utils/dbconnect.js
+++ utils/dbconnect.js
@@ -1,7 +1,7 @@
 const { MongoClient } = require('mongodb');
 
 //const connectionString = "mongodb://localhost:27017";
- const connectionString = "mongodb+srv://danih:danih@cluster0.kyiyu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+ const connectionString = "mongodb+srv://danih:danih@cluster0.kyiyu.mongodb.net/ekaly?retryWrites=true&w=majority";
 
 async function getDb(){
     return MongoClient.connect(connectionString, { useUnifiedTopology: true })
